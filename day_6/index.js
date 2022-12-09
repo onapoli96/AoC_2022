a2eafed14fdf48d202e23d042eab7edefb4a9e21
onapoli96 <onapoli1996@gmail.com>
2022-12-09 12:03:16 +0100

    Parte 2 e code clean

--- day_6/index.js
+++ day_6/index.js
@@ -12,10 +12,14 @@ const hasUniqueChars = (string) => {
   return true;
 };
 
-for (let i = 4; i < input.length; i++) {
-  const window = input.substring(i - 4, i);
-  if (hasUniqueChars(window)) {
-    console.log(i);
-    return;
+const findMarker = (windowSize) => {
+  for (let i = windowSize; i < input.length; i++) {
+    const window = input.substring(i - windowSize, i);
+    if (hasUniqueChars(window)) return i;
   }
-}
+};
+
+const response_1 = findMarker(4);
+const response_2 = findMarker(14);
+
+console.log(`Parte 1: ${response_1} Parte 2:  ${response_2} `);
